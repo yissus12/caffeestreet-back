8f1799e9d00184378ab230b7e348f95ca6c5a46c
“yissus12” <jessus13chu@gmail.com>
2025-10-28 14:47:41 -0500

    cambio nuevo

--- src/main/java/com/example/coffeeStret_Back/Controlador/ProductoControlador.java
+++ src/main/java/com/example/coffeeStret_Back/Controlador/ProductoControlador.java
@@ -3,6 +3,7 @@ package com.example.coffeeStret_Back.Controlador;
 import com.example.coffeeStret_Back.Entidades.Producto;
 import com.example.coffeeStret_Back.Servicio.ProductoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/producto")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductoControlador {
     @Autowired
     private ProductoServicio servicio;
